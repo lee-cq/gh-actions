be52921f39df37f1d91878e4ccf75e01e1e6b3be
LeeCQ <lee-cq@qq.com>
2023-03-21 00:02:08 +0800

    Update OneDrive mv rm upload_stream

--- actions/downloader/onedrive_upload.py
+++ actions/downloader/onedrive_upload.py
@@ -8,7 +8,7 @@ import subprocess
 import logging
 import urllib.parse
 from abc import ABC
-from pathlib import Path
+from pathlib import Path, PurePosixPath
 from typing import List
 
 import msal
@@ -35,6 +35,8 @@ class Ms365Client(ABC):
 
         self.scope = scope
         self.api_point = self.API_POINT_BETA if bate_api else self.API_POINT_V1
+
+        self.WORKDIR = '/'
 
         self.token_cache = msal.SerializableTokenCache()
         self.msal_app = msal.ConfidentialClientApplication(
@@ -98,7 +100,7 @@ class Ms365Client(ABC):
         else:
             raise ValueError('Can not get Account.')
 
-    def request(self, method, url, headers=None, **kwargs):
+    def request(self, method, url, headers=None, **kwargs) -> requests.Response:
         """
 
         :param method:
@@ -112,11 +114,12 @@ class Ms365Client(ABC):
 
         headers.setdefault('Authorization', f'Bearer {self.get_token_from_cache()}')
 
-        return requests.request(method=method, url=url, headers=headers, **kwargs).json()
+        return requests.request(method=method, url=url, headers=headers, **kwargs)
 
 
 class Onedrive(Ms365Client):
     scope = ['Files.ReadWrite.All', 'Sites.ReadWrite.All']
+    exception_char = r'\/|&?><;:\'"$'
 
     def __init__(self, client_id, client_secret, drive_type, drive_id=None):
         self.drive_id = drive_id
@@ -140,46 +143,218 @@ class Onedrive(Ms365Client):
             return _base_url
         raise TypeError(f'Not Support Drive. {self.drive_type}')
 
+    def quote_path(self, path) -> str:
+        """对 OneDrive 对路径进行编码
+
+        :param path:
+        :return:
+        """
+        if path == '/':
+            path = ''
+
+        if path and not path.startswith('/'):
+            raise ValueError('path 必须是绝对路径，所以需要以 / 开始。')
+
+        return self.api_point + '/root' + (f':{urllib.parse.quote(path)}:' if path else '')
+
+    @staticmethod
+    def assert_name(name) -> bool:
+        """判断itemName是否合法"""
+        for char in r'\/&|':
+            if char in name:
+                return False
+        return True
+
     def ls(self, path='') -> dict:
         """列出驱动器文件
 
         :param path: 驱动器中目录的绝对路径，只能是一个目录的路径。
         :rtype dict 该路径中的文件属性
         """
-        quote_path = f':{urllib.parse.quote(path)}:' if path else ''
 
-        url = self.api_point + '/root' + quote_path + '/children'
-        return self.request('GET', url)
+        url = self.quote_path(path) + '/children'
+        return self.request('GET', url).json()
 
-    def mkdir(self, dirname, parent_path='', exist='rename'):
+    def mkdir(self, dirname, parent_path='', conflict='fail') -> dict:
         """创建一个目录
 
         :param parent_path: 父路径
         :param dirname: 新的DirName
-        :param exist: 当dir已经存在时，如何处理？
+        :param conflict: 当dir已经存在时，如何处理？ fail (default) | replace | rename
         :return:
         """
-        quote_path = f':{urllib.parse.quote(parent_path)}:' if parent_path else ''
+        if not self.assert_name(dirname):
+            raise ValueError(f"dirname is error, can not have {self.exception_char}")
 
-        url = self.api_point + '/root' + quote_path + '/children'
+        url = self.quote_path(parent_path) + '/children'
         data = {
             "name": dirname,
             "folder": {},
-            "@microsoft.graph.conflictBehavior": exist
+            "@microsoft.graph.conflictBehavior": conflict
         }
-        return self.request('POST', url, json=data)
+        return self.request('POST', url, json=data).json()
 
-    def upload_stream(self, parent_id, data: bytes):
+    def rm(self, path) -> int:
+        """"""
+        url = self.quote_path(path)
+        return self.request('DELETE', url).status_code
+
+    def rename(self):
+        """rename"""
+
+    def mv(self):
+        """mv"""
+
+    def cp(self):
+        """CP"""
+
+    def upload_stream(self, file_name, parent_path='/', data: bytes = None) -> dict:
         """
 
-        :param parent_id: OneDrive Path
+        :param file_name:
+        :param parent_path: OneDrive Path
         :param data: 数据内容
         :return:
         """
-        url = self.api_point + '/items'
+        if not self.assert_name(file_name):
+            raise ValueError
 
-    def create_upload_session(self) -> str:
+        path = PurePosixPath(parent_path).joinpath(file_name).as_posix()
+
+        url = self.quote_path(path) + '/content'
+        return self.request('PUT', url, data=data).json()
+
+    def create_upload_session(self,
+                              file_name,
+                              parent_path='/',
+                              conflict='fail',
+                              defer_commit=False,
+                              description=''
+                              ) -> 'OnedriveUploadSession':
+        """创建一个上传大文件的Session
+
+        :param file_name: 文件名
+        :param parent_path: 父路径
+        :param conflict: 冲突处理，fail (default) | replace | rename
+        :param defer_commit: 延迟创建，在文件上传完之前，不创建文件。
+        :param description: 描述
+        """
+        url = self.quote_path(parent_path + f'{file_name}') + '/createUploadSession'
+
+        _session = OnedriveUploadSession(session_request=self.request, session_url=url, )
+        _session.set_remote_name(name=file_name)
+        _session.set_remote_conflict(conflict)
+        _session.set_remote_description(description)
+        _session.set_remote_defer(defer_commit)
+
+        return _session
+
+
+class OnedriveUploadSession:
+    MS_STEP_LEN = 327680
+
+    def __init__(self, session_request, session_url):
+        self.msal_request = session_request
+        self.url = session_url
+        self.session_body = dict()
+
+        self._session_created = False
+
+        self.upload_url = None
+        self.upload_expiration_time = None
+        self.upload_size = 0
+
+    def set_remote_conflict(self, conflict):
+        """定义远程冲突"""
+        if self._session_created:
+            raise
+        self.session_body['@microsoft.graph.conflictBehavior'] = conflict
+
+    def set_remote_name(self, name):
+        """定义远程名字"""
+        if self._session_created:
+            raise
+        self.session_body['name'] = name
+
+    def set_remote_defer(self, defer):
+        """定义是否需要在全部上传完成后再创建文件"""
+        if self._session_created:
+            raise
+        self.session_body['deferCommit'] = defer
+
+    def set_remote_size(self, size):
+        """定于 上传文件的大小"""
+        if self._session_created:
+            raise
+        self.upload_size = size
+        self.session_body['fileSize'] = size
+
+    def set_remote_description(self, desc):
+        """定义远程描述"""
+        if self._session_created:
+            raise
+        self.session_body['description'] = desc
+
+    def create_session(self, size=None):
         """"""
+        if size:
+            self.set_remote_size(size)
+
+        res: requests.Response = self.msal_request('POST', self.url, json=self.session_body)
+
+        if res.status_code != 200:
+            _j = res.json()
+            raise Exception(
+                f'code: {res.status_code}\n'
+                f'Error: {_j["error"]}'
+            )  # TODO
+
+        self._session_created = True
+        res: dict = res.json()
+
+        self.upload_url = res.get("uploadUrl")
+        self.upload_expiration_time = res.get('expirationDateTime')
+
+    def put_data(self, data: bytes, start_range: int = 0):
+
+        len_data = len(data)
+        if len_data < self.MS_STEP_LEN and len_data // self.MS_STEP_LEN != 0:
+            raise  # TODO
+
+        header = {'Content-Range': f'bytes {start_range}-{start_range + len_data - 1}/{self.upload_size}'}
+
+        requests.put(self.upload_url, headers=header, data=data)
+        logger.debug(f'Uploaded Range {start_range}-{start_range + len_data - 1}')
+
+    def cancel(self):
+        """取消上传"""
+        requests.delete(self.upload_url)
+
+
+    def from_file(self, file, remote_name=None):
+        file = Path(file)
+        if not file.is_file():
+            raise  # TODO
+
+        size = file.stat().st_size
+        if not remote_name:
+            self.set_remote_name(remote_name or file.name)
+
+        self.create_session(size)
+
+        with file.open('rb') as ff:
+            start_range = 0
+            step = self.MS_STEP_LEN
+            while True:
+                data = ff.read(step)
+                if not data:
+                    logger.debug('Break, ')
+                    break
+                self.put_data(data, start_range=start_range)
+                start_range += step - 1
+
+    def from_url(self, url):
+        pass
 
 
 if __name__ == '__main__':
@@ -201,4 +376,7 @@ if __name__ == '__main__':
     o = Onedrive(CLIENT_ID, CLIENT_SEC, os.getenv('MSAL_DRIVE_TYPE'), os.getenv('MSAL_DRIVE_ID_TEST'))
     o.load_token(refresh_token)
     print(o.get_refresh_token())
-    print(jsonify(o.mkdir('test', '/test')))
+    # print(jsonify(o.upload_stream('a.txt', data='s'.encode())))
+    session = o.create_upload_session('clash-darwin-arm64')
+    session
+    session.from_file('/Users/lcq/Downloads/clash-darwin-arm64')
--- test/test_onedrive.py
+++ test/test_onedrive.py
@@ -1,8 +1,8 @@
 # coding: utf8
 import logging
 import os
-
-import pytest
+import time
+import unittest
 
 from actions.downloader.onedrive_upload import Onedrive
 
@@ -11,12 +11,60 @@ from dotenv import load_dotenv
 load_dotenv()
 logging.basicConfig(level='DEBUG')
 
+unittest_dirname = 'unittest_dirname'
 
-def test_ls():
-    """测试ls"""
-    refresh_token = os.getenv('MSAL_ONEDRIVE_TOKEN')
-    client_id = os.getenv('MSAL_CLIENT_ID')
-    client_sec = os.getenv('MSAL_CLIENT_SECRET')
-    o = Onedrive(client_id, client_sec, os.getenv('MSAL_DRIVE_TYPE'), os.getenv('MSAL_DRIVE_ID_TEST'))
-    o.load_token(refresh_token)
-    print(o.ls())
+
+class TestOnedrive(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        refresh_token = os.getenv('MSAL_ONEDRIVE_TOKEN')
+        client_id = os.getenv('MSAL_CLIENT_ID')
+        client_sec = os.getenv('MSAL_CLIENT_SECRET')
+        onedrive = Onedrive(client_id, client_sec, os.getenv('MSAL_DRIVE_TYPE'), os.getenv('MSAL_DRIVE_ID_TEST'))
+        onedrive.load_token(refresh_token)
+        if unittest_dirname not in [i.get('name') for i in onedrive.ls().get('value', [])]:
+            onedrive.mkdir(unittest_dirname)
+
+        cls.onedrive = onedrive
+        cls.unittest_dirname = '/' + unittest_dirname
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        cls.onedrive.rm(cls.unittest_dirname)
+
+    def test_ls_root(self):
+        """测试ls"""
+        items = self.onedrive.ls()
+        self.assertNotIn('error', items)
+        self.assertIsInstance(items.get('value'), list)
+
+    def test_ls_test_dir(self):
+        items = self.onedrive.ls(self.unittest_dirname)
+        self.assertNotIn('error', items)
+        self.assertIsInstance(items.get('value'), list)
+
+    def test_mkdir(self):
+        """创建目录"""
+        dirname = f'test_{time.time()}'
+        res = self.onedrive.mkdir(dirname, parent_path=self.unittest_dirname)
+        self.assertNotIn('error', res)
+        self.assertEqual(res.get('name'), dirname)
+        return res['name']
+
+    def test_upload_file_small(self):
+        """上传小文件"""
+        file_name = f'test_file_{time.time()}.txt'
+        res = self.onedrive.upload_stream(file_name, parent_path=self.unittest_dirname, data=file_name.encode())
+        self.assertNotIn('error', res)
+        self.assertEqual(res.get('name'), file_name)
+        return res['name']
+
+    def test_rm_file(self):
+        name = self.test_upload_file_small()
+        res = self.onedrive.rm(self.unittest_dirname + f'/{name}')
+        self.assertEqual(res, 204)
+
+    def test_rm_dir(self):
+        name = self.test_mkdir()
+        res = self.onedrive.rm(self.unittest_dirname + f'/{name}')
+        self.assertEqual(res, 204)
