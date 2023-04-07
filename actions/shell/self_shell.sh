31c7cc76d97faba22b899173531f3d46626fff95
Lee CQ <47050568+lee-cq@users.noreply.github.com>
2023-04-07 16:57:09 +0800

    bugfix: shellrc

--- actions/shell/self_shell.sh
+++ actions/shell/self_shell.sh
@@ -1,9 +1,19 @@
 # 此文件将会在Shell创建时被加载
 # This File will be load when The shell is created.
 
-funcation _sec2min {
-    
+function sec2min() {
+    tms=$1
+    test -n "$tms" || read tms
+    test -n "$tms" || return 1
+
+    _h=$(($tms / 3600))  # 小时数
+    _m_s=$(($tms - _h * 3600))
+    _m=$(($_m_s / 60 ))
+    _s=$(($tms - $_m * 60 - $_h * 3600 ))
+
+    echo "$_h h $_m m $_s s"
+
 } 
 
 alias ll='ls -Ahl'
-alias stopout=$(cat /tmp/stopout | )
+alias stopout=$(cat /tmp/stopout | sec2min )
