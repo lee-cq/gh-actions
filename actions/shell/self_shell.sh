a76f1c0dae7d1ce726f208dc97870d6909bfa775
LeeCQ <lee-cq@qq.com>
2023-11-27 14:28:30 +0800

    更新Shell变量

--- actions/shell/self_shell.sh
+++ actions/shell/self_shell.sh
@@ -3,17 +3,18 @@
 
 function sec2min() {
     tms=$1
-    test -n "$tms" || read tms
+    test -n "$tms" || read -r tms
     test -n "$tms" || return 1
 
-    _h=$(($tms / 3600))  # 小时数
+    _h=$(($tms / 3600)) # 小时数
     _m_s=$(($tms - _h * 3600))
-    _m=$(($_m_s / 60 ))
-    _s=$(($tms - $_m * 60 - $_h * 3600 ))
+    _m=$(($_m_s / 60))
+    _s=$(($tms - $_m * 60 - $_h * 3600))
 
     echo "$_h h $_m m $_s s"
 
-} 
+}
 
 alias ll='ls -Ahl'
-alias stopout=$(cat /tmp/stopout | sec2min )
+alias stopout="cat /tmp/stopout | sec2min"
+alist stoprun="rm -f /tmp/keepalive && exit"
